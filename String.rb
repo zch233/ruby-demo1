9922b427219272f7c5f5ad1bb8b05e48252a5a41
zhangchao <529743595@qq.com>
2020-09-05 15:13:07 +0800

    Add Array

--- String.rb
+++ String.rb
@@ -4,3 +4,17 @@ p Object.superclass # BasicObject
 p BasicObject.superclass # nil
 
 p String.is_a? Class
+
+p '----Array----'
+p Array.superclass # Object
+p Array.is_a? Class
+
+class MyClass; end
+p MyClass.superclass # Object
+p MyClass.is_a? Class
+
+p 'class is a Class?'
+p Class.is_a? Class
+
+p Class.superclass # Module
+# 模块是低配版的类，类是高配版的模块
